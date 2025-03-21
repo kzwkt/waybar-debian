aee65b8beb2a9cfefdb06eb228a2cf8f77dd88e1
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-21 08:23:47 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 apt update 
-apt install wget  libgtkmm-3.0-dev libxkbregistry-dev libinput-dev libwayland-dev libwireplumber-0.4-dev pkgconf  cmake   gcc cmake ninja  meson ca-certificates binutils  -y
+apt install wget  libgtkmm-3.0-dev libxkbregistry-dev libinput-dev libwayland-dev libwireplumber-0.4-dev pkgconf  cmake   gcc cmake  meson ca-certificates binutils  -y
 version=0.12.0
 repo="Alexays/Waybar"
 echo $version > version 
 wget https://github.com/$repo/archive/refs/tags/$version.tar.gz
 tar -xzf $version.tar.gz
 cd Waybar-$version/
-meson setup build -Dlibnl=disabled -Ddbusmenu-gtk=disabled -Dupower_glib=disabled -Dcava=disabled -Dmpris=disabled -Dmpd=disabled -Dsndio=disabled -Dniri=false --wipe 
+meson setup build -Dlibnl=disabled -Ddbusmenu-gtk=disabled -Dupower_glib=disabled -Dcava=disabled -Dmpris=disabled -Dmpd=disabled -Dsndio=disabled -Dniri=false 
 ninja -C build/
 strip build/waybar
