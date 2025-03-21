9608eb0d2e42db3fb4d9b30342243978a5443c42
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-21 08:10:07 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,13 @@ apt update
 apt install -y ca-certificates  
 echo "deb-src https://deb.debian.org/debian stable main" |  tee -a /etc/apt/sources.list
 apt update
-apt install wget libmupdf-dev libwlroots-dev libxml2-dev libcairo2-dev libpango1.0-dev libpng-dev librsvg2-dev   gcc cmake  meson ca-certificates binutils  -y
-version=0.5.3
+apt install wget  libgtkmm-3.0-dev libxkbregistry-dev libinput-dev libwayland-dev libwireplumber-0.4-dev pkgconf systemd-dev cmake   gcc cmake  meson ca-certificates binutils  -y
+version=0.12.0
+repo="Alexays/Waybar"
 echo $version > version 
-wget https://github.com/labwc/labwc/archive/refs/tags/$version.tar.gz
+wget https://github.com/$repo/archive/refs/tags/$version.tar.gz
 tar -xzf $version.tar.gz
-cd labwc-$version/
-meson -Dxwayland=disabled build/
+cd Waybar-$version/
+meson setup build -Dlibnl=disabled -Ddbusmenu-gtk=disabled -Dupower_glib=disabled -Dcava=disabled -Dmpris=disabled -Dmpd=disabled -Dsndio=disabled -Dniri=false --wipe 
 ninja -C build/
-strip build/labwc
+strip build/waybar
